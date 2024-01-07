25d2a2d515ac951603ca0f5fdb5a23d62efbe178
HappyHyunJung <skytop4040@naver.com>
2024-01-07 23:36:19 +0900

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,4 @@
-const images = ["url(src/img/city.jpg)", "url(src/img/farm.jpg)", "url(src/img/rest.jpg)", "url(../img/store.jpg)"];
+const images = ["url(src/img/city.jpg)", "url(src/img/farm.jpg)", "url(src/img/rest.jpg)", "url(src/img/store.jpg)"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
